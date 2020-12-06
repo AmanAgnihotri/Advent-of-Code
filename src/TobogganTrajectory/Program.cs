5ee094cb313fd54452f7008d508d604d0f661874
AmanAgnihotri <aman.agnihotri@yahoo.com>
2020-12-06 17:54:09 +0530

    Refactor Toboggan Trajectory Program

--- src/TobogganTrajectory/Program.cs
+++ src/TobogganTrajectory/Program.cs
@@ -7,43 +7,26 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 
-namespace TobogganTrajectory
+const char tree = '#';
+
+var map = File.ReadAllLines("Input.txt")
+  .Select(line => new BitArray(line.Select(ch => ch == tree).ToArray()))
+  .ToImmutableList();
+
+Console.WriteLine(GetTreeCount(3, 1));
+
+Console.WriteLine(new[] {(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)}
+  .Select(pair => GetTreeCount(pair.Item1, pair.Item2))
+  .Select(Convert.ToInt64)
+  .Aggregate((a, b) => a * b));
+
+int GetTreeCount(int dx, int dy)
 {
-  public static class Program
-  {
-    public static void Main()
-    {
-      var map = GetMap();
+  var xc = map.First().Count;
+  var yc = map.Count / dy + map.Count % dy - 1;
 
-      Console.WriteLine(GetTreeCount(map, 3, 1));
+  var xs = Enumerable.Range(1, yc).Select(x => x * dx % xc);
+  var ys = Enumerable.Range(1, yc).Select(y => y * dy);
 
-      Console.WriteLine(new[] {(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)}
-        .Select(pair => GetTreeCount(map, pair.Item1, pair.Item2))
-        .Select(value => (long) value)
-        .Aggregate((a, b) => a * b));
-    }
-
-    private static int GetTreeCount(ImmutableList<BitArray> map, int dx, int dy)
-    {
-      var xc = map.First().Count;
-      var yc = map.Count / dy + map.Count % dy - 1;
-
-      var xs = Enumerable.Range(1, yc).Select(x => x * dx % xc);
-      var ys = Enumerable.Range(1, yc).Select(y => y * dy);
-
-      return xs.Zip(ys, (x, y) => map[y][x]).Count(v => v);
-    }
-
-    private static ImmutableList<BitArray> GetMap()
-    {
-      const char tree = '#';
-
-      return File.ReadAllLines("Input.txt")
-        .Select(AsBitArray)
-        .ToImmutableList();
-
-      static BitArray AsBitArray(string line) =>
-        new(line.Select(ch => ch == tree).ToArray());
-    }
-  }
+  return xs.Zip(ys, (x, y) => map[y][x]).Count(v => v);
 }
