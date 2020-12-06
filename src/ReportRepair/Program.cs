0f4d657f1c31acbd9c39eea41033d82b3f28396c
AmanAgnihotri <aman.agnihotri@yahoo.com>
2020-12-06 17:34:10 +0530

    Refactor Report Repair Program

--- src/ReportRepair/Program.cs
+++ src/ReportRepair/Program.cs
@@ -6,43 +6,23 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 
-namespace ReportRepair
+var numbers = File.ReadAllLines("Input.txt")
+  .Select(int.Parse)
+  .ToImmutableHashSet();
+
+Print(GetProduct(2020, 2));
+Print(GetProduct(2020, 3));
+
+int? GetProduct(int sum, int count, int product = 1) => count switch
 {
-  public static class Program
-  {
-    public static void Main()
-    {
-      var numbers = File.ReadAllLines("Input.txt")
-        .Select(int.Parse)
-        .ToImmutableHashSet();
+  < 1 => null,
+  1 when numbers.Contains(sum) => sum * product,
+  1 => null,
+  _ when sum < 1 => null,
+  _ => numbers
+    .Select(number => GetProduct(sum - number, count - 1, number * product))
+    .FirstOrDefault(result => result.HasValue)
+};
 
-      Print(GetProduct(numbers, 2020, 2));
-      Print(GetProduct(numbers, 2020, 3));
-    }
-
-    private static void Print(int? value) =>
-      Console.WriteLine(value is null ? "None" : value.ToString());
-
-    private static int? GetProduct(
-      IImmutableSet<int> numbers,
-      int sum,
-      int count,
-      int product = 1)
-    {
-      if (count < 1 || sum < 1) return null;
-
-      return count switch
-      {
-        1 when numbers.Contains(sum) => sum * product,
-        1 => null,
-        _ => numbers
-          .Select(number => GetProduct(
-            numbers.Except(new[] {number}),
-            sum - number,
-            count - 1,
-            number * product))
-          .FirstOrDefault(result => result.HasValue)
-      };
-    }
-  }
-}
+static void Print(int? value) =>
+  Console.WriteLine(value is null ? "None" : value.ToString());
