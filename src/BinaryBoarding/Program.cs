fac50a7d990ac7d73af47dd3cb8836545bca1ea1
AmanAgnihotri <aman.agnihotri@yahoo.com>
2020-12-10 19:47:46 +0530

    Refactor Binary Boarding Program

--- src/BinaryBoarding/Program.cs
+++ src/BinaryBoarding/Program.cs
@@ -6,9 +6,6 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-
-var regex = new Regex(@"^([FB]{7})([LR]{3})$", RegexOptions.Compiled);
 
 var seatIds = File.ReadLines("Input.txt")
   .Select(GetSeatId)
@@ -20,22 +17,14 @@ Console.WriteLine(Enumerable
   .Range(seatIds.Min(), seatIds.Count + 1)
   .SingleOrDefault(id => !seatIds.Contains(id)));
 
-int GetSeatId(string pass)
+static int GetSeatId(string pass)
 {
-  var groups = regex.Match(pass).Groups;
-
-  var rowInBinary = new StringBuilder(groups[1].Value)
+  var binary = new StringBuilder(pass)
     .Replace('F', '0')
     .Replace('B', '1')
-    .ToString();
-
-  var columnInBinary = new StringBuilder(groups[2].Value)
     .Replace('L', '0')
     .Replace('R', '1')
     .ToString();
 
-  var row = Convert.ToInt32(rowInBinary, 2);
-  var column = Convert.ToInt32(columnInBinary, 2);
-
-  return row * 8 + column;
+  return Convert.ToInt32(binary, 2);
 }
